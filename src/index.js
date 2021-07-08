3265a4a963f2e8df61a727bdce7560e5a68ec1c0
MHammadYou <MHammadY2.0@gmail.com>
2021-07-08 22:36:37 +0500

    Set up Electron and React with parcel.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>React-Electron</h1>, document.getElementById("app"));
+
+// try {
+//   require("electron-reloader")(module);
+// } catch (_) {}
