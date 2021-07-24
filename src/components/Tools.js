a00c6aa7bac4d9bb4e833a1eae867c273cf3b27f
MHammadYou <MHammadY2.0@gmail.com>
2021-07-24 22:54:20 +0500

    Added tools event handlers

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Marker, OilPaint, WaterColor, PixelPen, Pencil, Eraser, Crayon, SprayCan, ToolsHeading } from "./tools/index";
 import "../css/components/tools/layout.css"
 import "../css/components/tools/styles.css"
 
 function Tools() {
+
+  const [heading, setHeading] = useState("Marker");
+
+  const handleClick = newHeading => {
+    setHeading(newHeading);
+  }
+
   return (
     <div className={"tools"}>
       <div className="tools-heading-container">
-        <ToolsHeading heading={"Marker"}/>
+        <ToolsHeading heading={heading}/>
       </div>
       <div className="all-tools">
         <div className="tools-row-1">
           <Marker />
-          <OilPaint />
+          <OilPaint onClick={() => console.log("Working")}/>
           <WaterColor />
           <PixelPen />
         </div>
