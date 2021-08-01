d5d0d4d9f538a2a8f1523cba639e6f27278c6554
MHammadYou <MHammadY2.0@gmail.com>
2021-08-01 22:24:53 +0500

    added props input fields

--- src/components/properties/Opacity.js
+++ src/components/properties/Opacity.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Opacity() {
+
+  const [opacity, setOpacity] = useState(10);
+
+  const handleChange = (e) => {
+    console.log(e)
+  }
+
   return (
-    <div style={{"background-color": "red"}} className={"property"}>
+    <div style={{"backgroundColor": "red"}} className={"property"}>
       <div className="opacity-heading prop-heading">
         Opacity
+        <input type="text" defaultValue={opacity} className={"prop-input opacity-input"} onChange={handleChange}/>
       </div>
       <form action="" className={"opacity-slider prop-slider"}>
-        <input type="range" min="1" max="100" value="50" className="slider" id="myRange" />
+        <input type="range" min="1" max="100" defaultValue={opacity} className="slider"/>
       </form>
     </div>
   )
--- src/components/properties/Thickness.js
+++ src/components/properties/Thickness.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Thickness() {
+
+  const [thickness, setThickness] = useState(10);
+
+  const handleChange = (e) => {
+    console.log(e)
+  }
+
   return (
-    <div style={{"background-color": "blue"}} className={"property"}>
+    <div style={{"backgroundColor": "blue"}} className={"property"}>
       <div className="thickness-heading prop-heading">
         Thickness
+        <input type="text" defaultValue={thickness} className={"prop-input thickness-input"} onChange={handleChange}/>
       </div>
       <form action="" className={"thickness-slider prop-slider"}>
-        <input type="range" min="1" max="100" value="50" className="slider" id="myRange" />
+        <input type="range" min="1" max="100" defaultValue={thickness} className="slider"/>
       </form>
     </div>
   )
