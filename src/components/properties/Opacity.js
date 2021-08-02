0919ac3ca2a5121e5399d90953e98c937cf7d5b6
MHammadYou <MHammadY2.0@gmail.com>
2021-08-02 19:39:34 +0500

    Properties Slider and inputs now update the state

--- src/components/properties/Opacity.js
+++ src/components/properties/Opacity.js
@@ -6,17 +6,24 @@ export default function Opacity() {
   const [opacity, setOpacity] = useState(10);
 
   const handleChange = (e) => {
-    console.log(e)
+
+    if (e.target.value > 100) {
+      e.target.value = 100;
+    }
+    if (e.target.value < 1) {
+      e.target.value = 1;
+    }
+    setOpacity(e.target.value);
   }
 
   return (
     <div style={{"backgroundColor": "red"}} className={"property"}>
       <div className="opacity-heading prop-heading">
         Opacity
-        <input type="text" defaultValue={opacity} className={"prop-input opacity-input"} onChange={handleChange}/>
+        <input type="text" value={opacity} className={"prop-input opacity-input"} onChange={handleChange}/>
       </div>
       <form action="" className={"opacity-slider prop-slider"}>
-        <input type="range" min="1" max="100" defaultValue={opacity} className="slider"/>
+        <input type="range" min="1" max="100" value={opacity} className="slider" onChange={handleChange}/>
       </form>
     </div>
   )
--- src/components/properties/Thickness.js
+++ src/components/properties/Thickness.js
@@ -6,17 +6,24 @@ export default function Thickness() {
   const [thickness, setThickness] = useState(10);
 
   const handleChange = (e) => {
-    console.log(e)
+
+    if (e.target.value > 100) {
+      e.target.value = 100;
+    }
+    if (e.target.value < 1) {
+      e.target.value = 1;
+    }
+    setThickness(e.target.value);
   }
 
   return (
     <div style={{"backgroundColor": "blue"}} className={"property"}>
       <div className="thickness-heading prop-heading">
         Thickness
-        <input type="text" defaultValue={thickness} className={"prop-input thickness-input"} onChange={handleChange}/>
+        <input type="text" value={thickness} className={"prop-input thickness-input"} onChange={handleChange}/>
       </div>
       <form action="" className={"thickness-slider prop-slider"}>
-        <input type="range" min="1" max="100" defaultValue={thickness} className="slider"/>
+        <input type="range" min="1" max="100" value={thickness} className="slider" onChange={handleChange}/>
       </form>
     </div>
   )
