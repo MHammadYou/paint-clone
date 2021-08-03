509ec8170d8755289dcd054eebbf6b75d32b5b01
MHammadYou <MHammadY2.0@gmail.com>
2021-08-03 22:52:34 +0500

    Added colors layout

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { ColorEditor, EyeDropper, ColorsPalette, RecentColors } from "./colors/index";
+import "../css/components/colors/layout.css";
 
 function Colors() {
   return (
     <div style={{ backgroundColor: "green" }} className={"colors"}>
-      <ColorEditor />
-      <EyeDropper />
-      <ColorsPalette />
-      <RecentColors />
+      <div className="colors-top">
+        <div className="color-editor-container">
+          <ColorEditor />
+        </div>
+        <div className="eye-dropper-container">
+          <EyeDropper />
+        </div>
+      </div>
+      <div className="color-palette-container">
+        <ColorsPalette />
+      </div>
+      <div className="recent-colors-container">
+        <RecentColors />
+      </div>
     </div>
   );
 }
