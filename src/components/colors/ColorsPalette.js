8a678367eba2a540b3b4213c0eb506d8d1a5e22b
MHammadYou <MHammadY2.0@gmail.com>
2021-08-05 23:25:19 +0500

    Added color palettes

--- src/components/colors/ColorsPalette.js
+++ src/components/colors/ColorsPalette.js
@@ -4,18 +4,34 @@ import Palette from "./Palette";
 
 export default function ColorsPalette() {
 
-  const AllColors = ["black", "blue", "orange", "violet", "#897321"];
+  const AllColors = [
+    ["black", "blue", "orange", "violet", "#897321"],
+    ["#111", "#222", "#333", "#444", "#555", "#666"],
+    ["#111", "#222", "#333", "#444", "#555", "#666"],
+  ];
 
-  let elements = [];
+  const topColors = [];
+  const midColors = [];
+  const botColors = [];
 
-  for (let i = 0; i < AllColors.length; i++) {
-    elements.push(<Palette color={AllColors[i]} key={i}/>)
+  for (let i = 0; i < AllColors[0].length; i++) {
+    topColors.push(<Palette color={AllColors[0][i]} key={i}/>)
+    midColors.push(<Palette color={AllColors[1][i]} key={i}/>)
+    botColors.push(<Palette color={AllColors[2][i]} key={i}/>)
   }
-  console.log(elements)
+  console.log(topColors)
 
   return (
     <div className={"colors-palette"} style={{"backgroundColor": "blue"}}>
-      {elements}
+      <div className="top-colors">
+        {topColors}
+      </div>
+      <div className="mid-colors">
+        {midColors}
+      </div>
+      <div className="bot-colors">
+        {botColors}
+      </div>
     </div>
   )
 }
