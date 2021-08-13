4205470a77cc20ef27242ac4ef434745d55185b6
MHammadYou <MHammadY2.0@gmail.com>
2021-08-13 21:57:36 +0500

    Included contextAPI

--- src/components/colors/ColorEditor.js
+++ src/components/colors/ColorEditor.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 
 export default function ColorEditor(props) {
 
+  const curColorContext = React.createContext("#100");
+
   const [ currentColor, setCurrentColor ] = useState("#100");
 
   const styles = {
