823ef2bb68c9cbe873ceca90c73119a76b676bf3
MHammadYou <MHammadY2.0@gmail.com>
2021-08-15 23:21:02 +0500

    Added eyeDropper

--- src/components/colors/EyeDropper.js
+++ src/components/colors/EyeDropper.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function EyeDropper() {
+
+  const [ bgColor, setBgColor ] = useState("#000");
+
+  const handleChange = e => {
+    console.log(e.target.value);
+    setBgColor(e.target.value);
+  }
+
+  const styles = {
+    backgroundColor: bgColor
+  }
+
   return (
-    <div className={"eye-dropper"}>
-      EyeDropper
+    <div className={"eye-dropper"} style={styles}>
+      <input type="color" onChange={handleChange}/>
     </div>
   )
 }
