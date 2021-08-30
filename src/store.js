0944ac5ae03ea32da4fdb5806b334482d20e5778
MHammadYou <MHammadY2.0@gmail.com>
2021-08-30 21:14:01 +0500

    updated store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import colorsReducer from "./components/colors/colorsSlice"
+import opacityReducer from "./components/properties/opacitySlice"
+import thicknessReducer from "./components/properties/thicknessSlice"
 
 export default configureStore({
   reducer: {
-    colors: colorsReducer
+    colors: colorsReducer,
+    opacity: opacityReducer,
+    thickness: thicknessReducer
   },
 })
